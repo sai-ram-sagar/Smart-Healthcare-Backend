a090258b9db39c7096639c1566b1aa2dee210b8d
sai-ram-sagar <sairamsagar0912@gmail.com>
2025-03-10 16:28:59 +0530

    Fix CORS issue

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Change '*' to your frontend URL for security
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 app.use(bodyParser.json());
 
 // Database connections
