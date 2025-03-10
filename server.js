3cbd7bdf921b4121833ac6dc22e106a7e5279753
sai-ram-sagar <sairamsagar0912@gmail.com>
2025-03-10 16:45:33 +0530

    Fix CORS issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000/', // Change '*' to your frontend URL for security
+    origin: '*', // Change '*' to your frontend URL for security
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
